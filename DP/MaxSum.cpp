3915ccae977b279d4fc323ecce7ef3cf67b6558e
roliygu <roliygu@gmail.com>
2014-03-11 15:54:05 +0800

    DP_MaxSum_1.0

---
+++ DP/MaxSum.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+const int Max=1024;
+class MaxSum{
+	private:
+		int sum[Max],len,maxEndIndex,maxStartIndex,maxsum;
+	public:	
+		MaxSum(int *A,int L);
+		void setup();
+		void show();
+};
+MaxSum::MaxSum(int *A,int L){
+	//A元素从0开始 
+	len=L;
+	maxsum=0;
+	for(int i=1;i<=L;i++)
+		sum[i]=A[i-1];
+}
+void MaxSum::setup(){
+	int tempsum=0;
+	for(int i=1;i<=len;i++){
+		if(tempsum>0)
+			tempsum+=sum[i];
+		else
+			tempsum=sum[i];
+		if(tempsum>maxsum){
+			maxsum=tempsum;
+			maxEndIndex=i;
+		}	
+	}
+	int tempmax=maxsum;
+	for(int i=maxEndIndex;i>=1;i--){
+		if(tempmax-sum[i]==0)
+			maxStartIndex=i;
+		else
+			tempmax-=sum[i];
+	}
+}
+void MaxSum::show(){
+	for(int i=1;i<=len;i++)
+	cout<<sum[i]<<" ";
+	cout<<endl;
+	cout<<"the sum of ["<<maxStartIndex<<','<<maxEndIndex<<"] "<<maxsum<<" is the maximum."<<endl;
+}
+int main(){
+	int A[10]={-2,11,-4,13,-5,-2};
+	MaxSum B(A,6);
+	B.setup();
+	B.show();
+	return 0;
+}
