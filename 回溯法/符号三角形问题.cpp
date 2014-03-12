fdab224bebb3926953206b7177f43e9a19d4335d
roliygu <roliygu@gmail.com>
2014-03-12 16:09:52 +0800

    Backtreck_1.1

--- 回溯法/符号三角形问题.cpp
+++ 回溯法/符号三角形问题.cpp
@@ -3,45 +3,44 @@ using namespace std;
 const int Max=256;
 class Triangle{
 	private:
-		int sum,len,half;
+		int sum,len,half,count;
 		int T[Max][Max];
 	public:
 		Triangle(int n);
-		void Backtrack(int i,in count);
+		void Backtrack(int i,int count);
 		void show();
 };
 Triangle::Triangle(int n){
 	memset(T,0,sizeof(int)*Max*Max);
 	len=n;
 	int temp = (1+n)*n/2;
-	if(temp%4!=0)
+	if(temp%2!=0)
 		cout<<"不能构成三角"<<endl;
 	else
 		sum=0;
-	half=temp/4; 
+	half=temp/2; 
 }
-void Triangle::Backtrack(int i,int count){
-	if(count>half||(i*(i-1))/2-count>half)
-		return;
-	if(i>len)
+void Triangle::Backtrack(int t,int count){
+	if((count>half)||(t*(t-1)/2-count)>half)return ;
+	if(t>len)
 		sum++;
-	else{
+	else
 		for(int i=0;i<2;i++){
-			T[1][i]=i;
-			int newcount = count+1;
-			for(int j=2;j<=i;j++){
-				T[j][i-j+1]=T[j-1][i-j+1]^T[j-1][i-j+2];
-				newcount+=T[i][i-j+1];
+			T[1][t]=i;
+			int newcount = count+i;
+			for(int j=2;j<=t;j++){
+				T[j][t+1-j]=T[j-1][t+1-j]^T[j-1][t+2-j];
+				newcount+=T[j][t+1-j];
 			}
-			Backtrack(i+1,newcount);
+			Backtrack(t+1,newcount);
 		}
-	}
 }
 void Triangle::show(){
 	cout<<"count :"<<sum<<endl;
+	cout<<len;
 }
 int main(){
-	Triangle A(7);
+	Triangle A(4);
 	A.Backtrack(1,0);
 	A.show();
 	return 0;
